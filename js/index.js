f6af52598fc28110d56b3b0619b963ca7f641a6a
Jawad Hussein <jawaddhussein@gmail.com>
2019-01-28 15:29:01 -0800

    All complete except images

--- js/index.js
+++ js/index.js
@@ -40,3 +40,45 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+
+let navs = document.querySelectorAll('header nav a');
+
+navs[0].textContent = 'Services';
+navs[1].textContent = 'Product';
+navs[2].textContent = 'Vision';
+navs[3].textContent = 'Features';
+navs[4].textContent = 'About';
+navs[5].textContent = 'Contact';
+
+
+let subtitles = document.querySelectorAll('div h4');
+
+subtitles[0].textContent = 'Features';
+subtitles[1].textContent = 'About';
+subtitles[2].textContent = 'Services';
+subtitles[3].textContent = 'Product';
+subtitles[4].textContent = 'Vision';
+subtitles[5].textContent = 'Contact';
+
+let h1 = document.querySelector('section div h1');
+
+h1.textContent = 'DOM Is Awesome';
+
+let button = document.querySelector('button');
+
+button.textContent = 'Get Started';
+
+let ptags = document.querySelectorAll('p');
+
+ptags[0].textContent = 'Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+ptags[1].textContent = 'About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+ptags[2].textContent = 'Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+ptags[3].textContent = 'Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+ptags[4].textContent = 'Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+ptags[5].textContent = '123 Way 456 Street Somewhere, USA';
+ptags[6].textContent = '1 (888) 888-8888';
+ptags[7].textContent = 'sales@greatidea.io';
+ptags[8].textContent = 'Copyright Great Idea! 2018';
+
+
